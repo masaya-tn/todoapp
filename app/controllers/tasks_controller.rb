455e9b9af5bc974456290d87d632c457ea6ab7f7
masaya-tn <uvermasayan@icloud.com>
2020-06-15 16:52:53 +0900

    eyecatch

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,7 +19,7 @@ class TasksController < ApplicationController
 
     private
     def task_params
-      params.require(:task).permit(:title, :content, :limit, :user_id)
+      params.require(:task).permit(:title, :content, :limit, :user_id, :eyecatch)
     end
 end
     
