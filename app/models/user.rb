17b1f572cb3b660ba70f072bc35393aa43a5a39d
masaya-tn <uvermasayan@icloud.com>
2020-06-16 22:00:50 +0900

    profile作り出し

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  
+
   has_many :boards, dependent: :destroy
   has_many :tasks, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_one :profile, dependent: :destroy
 
   
 end
